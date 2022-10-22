30482fb409058abf173e0f365201b72c24718b58
Cha <cktmddus11@naver.com>
2022-10-22 23:37:10 +0900

    chapter02 commit

--- ch02-jpa-start1/src/main/java/jpabook/start/Member.java
+++ ch02-jpa-start1/src/main/java/jpabook/start/Member.java
@@ -6,12 +6,12 @@ import javax.persistence.*;  //**
  * User: HolyEyE
  * Date: 13. 5. 24. Time: 오후 7:43
  */
-@Entity
-@Table(name="MEMBER")
+@Entity          // 이 클래스를 테이블과 매핑한다.
+@Table(name="MEMBER") // 매핑할 테이블 정보를 알려줌.
 public class Member {
 
-    @Id
-    @Column(name = "ID")
+    @Id // 기본키 - 식별자 필드
+    @Column(name = "ID") // 필드를 컬럼에 맵핑
     private String id;
 
     @Column(name = "NAME")
